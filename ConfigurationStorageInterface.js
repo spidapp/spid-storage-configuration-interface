12937f9b87a92f0cd89734f6e018132983feec3d
FG Ribreau <github@fgribreau.com>
2014-11-07 23:38:36 +0100

    write and read now supports `prefix` and `properties`

--- ConfigurationStorageInterface.js
+++ ConfigurationStorageInterface.js
@@ -4,7 +4,7 @@ var Interface = require('spid-interface');
 
 module.exports = Interface('ConfigurationStorageInterface', {
   /**
-   * Load the configuration storage plugin
+   * init the configuration storage plugin
    * @param  {Configuration} configuration
    * @param  {Function} f(err)
    */
@@ -17,41 +17,45 @@ module.exports = Interface('ConfigurationStorageInterface', {
   dispose: function (f) {},
 
   /**
-   * Read a `key` from the configuration storage
-   * @param  {String} key
-   * @param  {Function} f(err, value: Mixed)
+   * Read some `keys` from the configuration storage
+   * @param  {String} prefix
+   * @param  {Array[String]} keys
+   * @param  {Function} f(err, properties: Object)
    */
-  read: function (key, f) {},
+  read: function (prefix, keys, f) {},
 
   /**
    * Write a `key` with `value` in the configuration storage
-   * @param  {String} key  [description]
-   * @param  {Mixed} value [description]
+   * @param  {String} prefix
+   * @param  {Object} properties e.g. {key1: value1, key2: value2}
    * @param  {Function} f(err)
    */
-  write: function (key, value, f) {},
+  write: function (prefix, properties, f) {},
 
   /**
    * Remove a `key` from the configuration storage
-   * @param  {String} key  [description]
+   * @param  {String} prefix
+   * @param  {Array[String]} keys
    * @param  {Function} f(err)
    */
-  remove: function (key, f) {},
+  remove: function (prefix, keys, f) {},
 
   /**
    * Watch keys for change
+   * @param {String} prefix
    * @param {Array[String]} keys array of keys to watch
    * @param {Function} f(key, newValue)
    */
-  watch: function(keys, f){},
+  watch: function (prefix, keys, f) {},
 
   /**
    * Unwatch keys
+   * @param  {String} prefix
    * @param  {Array[String]} keys
    * @optional
    * @param  {Function} f(err)
    * @optional
    * If no parameters were given, all listeners will be removed
    */
-  unwatch: function(keys, f){}
+  unwatch: function (prefix, keys, f) {}
 });
